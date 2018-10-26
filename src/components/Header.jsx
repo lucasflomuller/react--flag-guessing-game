dc4f357bf8657bc1206fe645ee4502083911fb34
lucasflomuller <lucasflorianomuller@gmail.com>
2018-10-26 00:37:48 +0000

    change header class component to functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import "../styles/Header.css";
 
-class Header extends Component {
-  render() { 
-    return ( 
-      <header>
-        <h1>Super Ultra Flag Guessing Game!</h1>
-      </header>
-     );
-  }
-}
- 
+const Header = () => {
+  return (
+    <header>
+      <h1>Super Ultra Flag Guessing Game!</h1>
+    </header>
+  );
+} 
+
 export default Header;
