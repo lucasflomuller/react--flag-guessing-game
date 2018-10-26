54b8a75668669d56c1d7ba51b43fcdd321e3037f
lucasflomuller <lucasflorianomuller@gmail.com>
2018-10-26 00:17:50 +0000

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+class Header extends Component {
+  render() { 
+    return ( 
+      <header>
+        <h1>Super Ultra Flag Guessing Game!</h1>
+      </header>
+     );
+  }
+}
+ 
+export default Header;
