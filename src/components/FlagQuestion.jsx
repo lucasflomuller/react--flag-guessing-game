4aa025fac24b44f86ccb942347fe057c93a4f200
lucasflomuller <lucasflorianomuller@gmail.com>
2018-11-01 23:43:45 +0000

    initial commit on flag question

---
+++ src/components/FlagQuestion.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class FlagQuestion extends Component {
+  render() { 
+    return ( <div>FlagQuestion</div> );
+  }
+}
+ 
+export default FlagQuestion;
--- src/components/GuessingGame.jsx
+++ src/components/GuessingGame.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import shuffle from 'shuffle-array';
+import React, { Component } from "react";
+import shuffle from "shuffle-array";
+import FlagQuestion from "./FlagQuestion";
 import "../styles/GuessingGame.css";
 
 class GuessingGame extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -14,25 +14,26 @@ class GuessingGame extends Component {
       countries: []
     };
   }
-  
+
   componentDidMount() {
     fetch("https://restcountries.eu/rest/v2/all")
-    .then(data => data.json())
-    .then(
-      (countries) => {
-      const correctAnswer = Math.floor(Math.random() * countries.length);
-      const options = this._getOptions(correctAnswer, countries);
-      this.setState({
-        correctAnswer,
-        options,
-        countries
-      });
-      console.log(this.state)
-    },
-    // Handling error
-    (error) => {
-      this.setState({ error });
-    })
+      .then(data => data.json())
+      .then(
+        countries => {
+          const correctAnswer = Math.floor(Math.random() * countries.length);
+          const options = this._getOptions(correctAnswer, countries);
+          this.setState({
+            correctAnswer,
+            options,
+            countries
+          });
+          console.log(this.state);
+        },
+        // Handling error
+        error => {
+          this.setState({ error });
+        }
+      );
   }
 
   _getOptions(correctOption, countries) {
@@ -40,7 +41,7 @@ class GuessingGame extends Component {
     let tries = 0;
     while (options.length < 4 && tries < 15) {
       let option = Math.floor(Math.random() * countries.length);
-      if (options.indexOf(option) === -1 ) {
+      if (options.indexOf(option) === -1) {
         options.push(option);
       } else {
         tries++;
@@ -49,9 +50,24 @@ class GuessingGame extends Component {
     return shuffle(options);
   }
 
-  render() { 
-    return ( <div>GuessingGame</div> );
+  render() {
+    const { error, countries, options, correctAnswer, answerText } = this.state;
+    let output = <div>Loading...</div>;
+    if (error) {
+      output = <h1>Error, please try again</h1>;
+    } else {
+      if (correctAnswer !== undefined) {
+        const { flag, name } = countries[correctAnswer];
+        const opts = options.map(opt => ({
+          id: opt,
+          name: countries[opt].name
+        }));
+        output = <FlagQuestion answerText={name} />;
+      }
+    }
+
+    return output;
   }
 }
- 
+
 export default GuessingGame;
